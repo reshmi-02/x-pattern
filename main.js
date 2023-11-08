dffc10dfb2d84042099c84e7676f6254aeabe6d8
reshmi-02 <reshmihash123@gmail.com>
2023-11-08 09:31:36 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,33 @@
+let n=parseInt(prompt("Enter the number"));
+for(let i=1;i<=n;i++){
+    
+        for(let j=2;j<=i;j++){
+            document.write("&nbsp&nbsp");
+        }
+        for(let k=n;k>=i;k--){
+        if(k==n || k==i){
+            document.write("*&nbsp&nbsp")
+            }
+            else{
+                document.write("&nbsp&nbsp&nbsp&nbsp")
+            }
+        }
+    
+    document.write("<br>");
+}
+for(let i=1;i<=n;i++){
+   
+        for(let j=(n-1);j>=i;j--){
+        
+            document.write("&nbsp&nbsp");
+        }
+        for(let k=1;k<=i;k++){
+            if(k==1 || k==i){
+            document.write("*&nbsp&nbsp");
+            }
+            else{
+                document.write("&nbsp&nbsp&nbsp&nbsp");
+            }
+        }
+    document.write("<br>");
+}
